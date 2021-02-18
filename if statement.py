992547ed02f629dbc1c6dca736fa40f9856c3c5d
maslah <maslah7348@gmail.com>
2021-02-18 06:39:40 +0300

    commit to master

---
+++ if statement.py
@@ -0,0 +1,27 @@
+
+
+is_male = False
+is_tall = False
+
+if is_male and is_tall:
+    print("you are a tall male")
+
+elif is_male and not (is_tall):
+    print("You are  a short male")
+elif not(is_male) and is_tall:
+    print("you are not a male but are Tall")
+else:
+    print("you are either not male or not tall or both ")
+
+
+
+
+
+
+
+
+
+
+
+    
+
